e1a403827b5e2a8112a0a8c98306a73f7e06b116
het patel <hppatidar12d@gmail.com>
2025-01-01 23:20:21 +0530

    update work

--- dist/config/tokens.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tokenTypes = {
-    ACCESS: "access",
-    REFRESH: "refresh",
-};
-exports.default = tokenTypes;
